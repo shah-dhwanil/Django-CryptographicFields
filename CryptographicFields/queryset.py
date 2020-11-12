b8a8e2c2ab017f0fe44c463e6c8dc127efc5db13
Shahprogrammer <71475691+Shahprogrammer@users.noreply.github.com>
2020-11-12 12:57:52 +0530

    Added docstring for queryset.py
    Added docstrings for queryset.py

--- CryptographicFields/queryset.py
+++ CryptographicFields/queryset.py
@@ -1,8 +1,16 @@
 class CryptographicQuerySet():
-    def __init__(self,queryset) -> None:
-        self.queryset=queryset
+    """
+    Class for storing results of query.
+
+    Class for storing results of query after filtering or sorting query results 
+    """
+
+    def __init__(self, queryset) -> None:
+        self.queryset = queryset
         return None
+
     def __iter__(self):
         return iter(self.queryset)
+
     def __repr__(self):
         return '<%s %r>' % (self.__class__.__name__, self.queryset)
