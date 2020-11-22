8acf238f27eda5311c5229fa59ae9f43dd024219
Shahprogrammer <71475691+Shahprogrammer@users.noreply.github.com>
2020-11-22 18:46:44 +0530

    updated docs settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ release = '2.0.0'
 
 
 # -- General configuration ---------------------------------------------------
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
