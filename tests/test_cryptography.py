87d40fd601a513e1ab27b1cd54faacab2fda4c25
Shahprogrammer <shahprogrammer05@gmail.com>
2020-11-24 17:00:20 +0530

    Update test_cryptography.py

--- tests/test_cryptography.py
+++ tests/test_cryptography.py
@@ -24,10 +24,22 @@ class Cryptography(unittest.TestCase):
         from uuid import uuid5,NAMESPACE_URL
         self.assertEqual(type_check("CryptographicFields"),bytearray("CryptographicFields","UTF-8"))
         self.assertEqual(type_check(b"CryptographicFields"),bytearray(b"CryptographicFields"))
+        self.assertEqual(type_check(bytearray("CryptographicFields","UTF-8")),bytearray("CryptographicFields","UTF-8"))
         self.assertEqual(type_check(149),bytearray(str(149),"UTF-8"))
+        self.assertEqual(type_check(True),bytearray(str(True),"UTF-8"))
         self.assertEqual(type_check(149.250),bytearray(str(149.250),"UTF-8"))
         self.assertEqual(type_check(datetime.now()),bytearray(str(datetime.now()),"UTF-8"))
         self.assertEqual(type_check(uuid5(NAMESPACE_URL,"www.google.com")),bytearray(str('c74a196f-f19d-5ea9-bffd-a2742432fc9c'),"UTF-8"))
 
+    def test_to_hex(self):
+        self.assertEqual(to_hex(b'CryptographicFields'),"43727970746f677261706869634669656c6473")
+    def test_from_hex(self):
+        self.assertEqual(from_hex("43727970746f677261706869634669656c6473"),bytearray(b'CryptographicFields'))
+    def test_encrypt(self):
+        setattr(settings,'CRYPTOGRAPHY_KEY',"XmeExmMq5tx1u9fbgLTKr26toW58OeC2sNvy1BEazSDBobWj0areehwvRUif")
+        self.assertEqual(encrypt("CryptographicFields"),"3580fc0d7b8c0790ec07786bc7f4ba3787f8a3")
+    def test_decrypt(self):
+        setattr(settings,'CRYPTOGRAPHY_KEY',"XmeExmMq5tx1u9fbgLTKr26toW58OeC2sNvy1BEazSDBobWj0areehwvRUif")
+        self.assertEqual(decrypt("3580fc0d7b8c0790ec07786bc7f4ba3787f8a3"),"CryptographicFields")
 if __name__ == '__main__':
     unittest.main()
