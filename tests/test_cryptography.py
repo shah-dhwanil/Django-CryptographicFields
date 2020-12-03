e6aee517995e959e4220124093a6bcb8dba0574d
Shahprogrammer <shahprogrammer05@gmail.com>
2020-12-03 15:16:51 +0530

    Update test_cryptography.py

--- tests/test_cryptography.py
+++ tests/test_cryptography.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from CryptographicFields.cryptography import *
 
 class Cryptography(unittest.TestCase):
-    settings.configure(CRYPTOGRAPHY_KEY="",SECRET_KEY="")
     def test_get_key_prefrence(self):
         setattr(settings,'CRYPTOGRAPHY_KEY',"XmeExmMq5tx1u9fbgLTKr26toW58OeC2sNvy1BEazSDBobWj0areehwvRUif")
         setattr(settings,"SECRET_KEY","98sgfit47#^&%dkfgfylaffsfvsk)w9bysdfv;[fgyeva47054^*7fuzgf7*^E7bjfae7be7")
