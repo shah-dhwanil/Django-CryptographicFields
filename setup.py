a2dce9e0cdba51113b73f2ea6c9d98086a70788b
Shahprogrammer <shahprogrammer05@gmail.com>
2020-12-07 08:11:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 import setuptools
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Django-CryptographicFields", # Replace with your own username
-    version="2.0.0",
+    name="Django-CryptographicFields",
+    version="2.0.1",
     author="Shahprogrammer",
     author_email="shahprogrammer05@gmail.com",
     description="A Django app for cryptography in Django Models.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Shahprogrammer/Django-CryptographicFields",
-    packages=setuptools.find_packages(),
+    packages=["CryptographicFields"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
